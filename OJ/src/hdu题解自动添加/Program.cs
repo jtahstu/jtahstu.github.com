133ae382afa6f94265da1704f5df3d5189ded34c
jtahstu <1373758426@qq.com>
2015-08-13 18:20:50 +0800

    2015/8/13 oj题解重大更新
    加入杭电两千多题题解

---
+++ OJ/src/hdu题解自动添加/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace hdu题解自动添加 {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 4365; i>=1000; i--) {
+                var utf8WithBom = new System.Text.UTF8Encoding(true);
+                string index = @"D:\Github\jtahstu.github.com\OJ\index.html";
+                StreamReader cin = new StreamReader(index, utf8WithBom);
+                string s = cin.ReadToEnd();
+                cin.Close();
+                string file = "P" + i.ToString();
+                string str = "<a href=\"code\\hdu\\" + file + ".html\"" + " target=\"_blank\">" + file + "&nbsp;&nbsp;</a>";
+                if (i % 14 == 0) str = str + "<br />";
+                int start = s.IndexOf("<p id=\"insert\">") + 15;
+                s = s.Insert(start, str);//插入链接
+
+                StreamWriter readTxt = new StreamWriter(index, false, utf8WithBom);
+                readTxt.Write(s);
+                readTxt.Close();
+            }
+        }
+    }
+}
---
+++ OJ/src/hdu题解自动生成/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace hdu题解自动生成 {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 1000; i < 4366; i++) {
+                try {
+                    string path = @"G:\ACM\OJ\code\hduoj_accept\hduoj_" + i.ToString() + ".cpp";
+                    StreamReader sr = new StreamReader(path);
+                    string s_path = sr.ReadToEnd();
+                    sr.Close();
+                    string te_path = @"D:\Github\jtahstu.github.com\OJ\code\hdu\Te.html";
+                    StreamReader sr2 = new StreamReader(te_path);
+                    string s_te = sr2.ReadToEnd();
+                    sr2.Close();
+                    s_te = s_te.Replace("$code$", s_path);
+                    s_te = s_te.Replace("$time$", DateTime.Now.ToString());
+                    s_te = s_te.Replace("$title$", "HDUOJ_P" + i.ToString());
+                    string code_path = @"D:\Github\jtahstu.github.com\OJ\code\hdu\P" + i.ToString() + ".html";
+                    StreamWriter readTxt = new StreamWriter(code_path, false);
+                    readTxt.Write(s_te);
+                    readTxt.Close();
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+        }
+    }
+}
