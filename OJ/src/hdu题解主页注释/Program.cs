8c1d960144d5a109c0f2d9c546a19974d24b808d
jtahstu <1373758426@qq.com>
2015-08-16 21:53:13 +0800

    2015/8/16 更新维护

---
+++ OJ/src/hdu题解主页注释/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace hdu题解主页注释 {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 4366; i <= 5299; i++) {
+                try {
+                    string path = @"D:\Github\jtahstu.github.com\OJ\code\hdu\P" + i.ToString()+".html";
+                    if (!File.Exists(path)) {//如果文件不存在，注释掉主页对应的行号
+                        String path_index=@"D:\Github\jtahstu.github.com\OJ\index.html";
+                        StreamReader sr = new StreamReader(path_index);
+                        string s_index = sr.ReadToEnd();
+                        sr.Close();
+                        int start = s_index.IndexOf("P" + i.ToString() + "&nbsp;&nbsp;");
+                        string s_in = "<!-- ", s_in2 = " -->";
+                        s_index = s_index.Insert(start , s_in);
+                        s_index = s_index.Insert(start +22, s_in2);
+                        File.Delete(path_index);
+                        StreamWriter wr = new StreamWriter(path_index,false);
+                        wr.WriteLine(s_index);
+                        wr.Close();
+                    }
+
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+            }
+        }
+    }
+}
--- OJ/src/hdu题解自动添加/Program.cs
+++ OJ/src/hdu题解自动添加/Program.cs
@@ -8,21 +8,27 @@ using System.IO;
 namespace hdu题解自动添加 {
     class Program {
         static void Main(string[] args) {
-            for (int i = 4365; i>=1000; i--) {
-                var utf8WithBom = new System.Text.UTF8Encoding(true);
-                string index = @"D:\Github\jtahstu.github.com\OJ\index.html";
-                StreamReader cin = new StreamReader(index, utf8WithBom);
-                string s = cin.ReadToEnd();
-                cin.Close();
-                string file = "P" + i.ToString();
-                string str = "<a href=\"code\\hdu\\" + file + ".html\"" + " target=\"_blank\">" + file + "&nbsp;&nbsp;</a>";
-                if (i % 14 == 0) str = str + "<br />";
-                int start = s.IndexOf("<p id=\"insert\">") + 15;
-                s = s.Insert(start, str);//插入链接
+            for (int i = 4366; i <= 5299; i++) {
+                try {
+                    var utf8WithBom = new System.Text.UTF8Encoding(true);
+                    string index = @"D:\Github\jtahstu.github.com\OJ\index.html";
+                    StreamReader cin = new StreamReader(index, utf8WithBom);
+                    string s = cin.ReadToEnd();
+                    cin.Close();
+                    string file = "P" + i.ToString();
+                    string str = "<a href=\"code\\hdu\\" + file + ".html\"" + " target=\"_blank\">" + file + "&nbsp;&nbsp;</a>";
+                    if (i % 14 == 0) str = str + "<br />";
+                    int start = s.IndexOf("<p id=\"insert\">");
+                    int start2 = s.IndexOf("</p>", start + 100);
+                    s = s.Insert(start2-1, str);//插入链接
 
-                StreamWriter readTxt = new StreamWriter(index, false, utf8WithBom);
-                readTxt.Write(s);
-                readTxt.Close();
+                    StreamWriter readTxt = new StreamWriter(index, false, utf8WithBom);
+                    readTxt.Write(s);
+                    readTxt.Close();
+                } catch (Exception e) {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
             }
         }
     }
